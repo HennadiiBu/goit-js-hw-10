1aad75e289b98038ea41af279226c6298f2c5588
Hennadii Bublikov <bublikov.gennadiy@gmail.com>
2023-04-24 00:16:25 +0300

    теперь нормально выводит языки

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ function onUserInput(event) {
   fetchCountries(event.target.value)
     .then(data => {
       const capitals = data.map(elem => elem.name.official);
+
       if (capitals.length > 2 && capitals.length < 10) {
         const marckupCountyList = data.map(
           elem =>
@@ -23,12 +24,12 @@ function onUserInput(event) {
       } else if ((capitals.length = 1)) {
         const markupCountyList = data.map(
           elem =>
-            `<li><img src="${elem.flags.svg}" alt="${elem.name.official} width="50px" height="50px"><h1>${elem.name.official}</h1></li>`
+            `<li><img src="${elem.flags.svg}" alt="${elem.name.official} width="100px" height="100px"><h1>${elem.name.official}</h1></li>`
         );
         const markUpCountryInfo = data.map(
           elem => ` <h2>Capital: ${elem.capital}</h2>
                     <h2>Population: ${elem.population}</h2>
-                    <h2>Languages: ${elem.languages}</h2>`
+                    <h2>Languages: ${Object.values(elem.languages)}</h2>`
         );
         countryList.insertAdjacentHTML('beforeend', markupCountyList.join(''));
         countryInfo.insertAdjacentHTML('beforeend', markUpCountryInfo.join(''));
@@ -37,7 +38,7 @@ function onUserInput(event) {
       //   console.log(capitals.length);
       // console.log(data.map(elem => elem.capital));
       //   console.log(data.map(elem => elem.population));
-      //   console.log(data.map(elem => elem.flags.svg));
+      console.log(data.map(elem => elem.languages));
     })
     .catch(err => console.log(err));
 }
