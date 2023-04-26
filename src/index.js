ba6943fd485dd4d5065ed026fd40d1ccbc6eabc0
Hennadii Bublikov <99074724+HennadiiBu@users.noreply.github.com>
2023-04-26 10:50:08 +0300

    done without css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
 import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -10,18 +11,22 @@ const userSeach = document.querySelector('#search-box');
 userSeach.addEventListener('input', debounce(onUserInput, DEBOUNCE_DELAY));
 
 function onUserInput(event) {
-  console.log(event.target.value);
   fetchCountries(event.target.value)
     .then(data => {
       const capitals = data.map(elem => elem.name.official);
 
       if (capitals.length > 2 && capitals.length < 10) {
+        countryList.innerHTML='';
+
         const marckupCountyList = data.map(
           elem =>
             `<li><img src="${elem.flags.svg}" alt="${elem.name.official} width="50px" height="50px"><h1>${elem.name.official}</h1></li>`
         );
         countryList.insertAdjacentHTML('beforeend', marckupCountyList.join(''));
       } else if ((capitals.length = 1)) {
+        countryInfo.innerHTML='';
+        countryList.innerHTML='';
+
         const markupCountyList = data.map(
           elem =>
             `<li><img src="${elem.flags.svg}" alt="${elem.name.official} width="100px" height="100px"><h1>${elem.name.official}</h1></li>`
@@ -33,12 +38,9 @@ function onUserInput(event) {
         );
         countryList.insertAdjacentHTML('beforeend', markupCountyList.join(''));
         countryInfo.insertAdjacentHTML('beforeend', markUpCountryInfo.join(''));
+      }else{
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.')
       }
-
-      //   console.log(capitals.length);
-      // console.log(data.map(elem => elem.capital));
-      //   console.log(data.map(elem => elem.population));
-      console.log(data.map(elem => elem.languages));
     })
-    .catch(err => console.log(err));
+    .catch(err => Notiflix.Notify.failure('Oops, there is no country with that name'));
 }
