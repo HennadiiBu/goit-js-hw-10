7beecd7af6d0a6b95e23e1a9d0593e56d7b4b068
Hennadii Bublikov <99074724+HennadiiBu@users.noreply.github.com>
2023-04-30 16:09:56 +0300

    исправление ошибок

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,7 +2,9 @@ const BASE_URL = 'https://restcountries.com/v3.1';
 const END_POINT = `/name/`;
 
 function fetchCountries(name) {
-  return fetch(`${BASE_URL}${END_POINT}${name}`).then(resp => {
+  return fetch(
+    `${BASE_URL}${END_POINT}${name}?fields=name,capital,population,flags,languages`
+  ).then(resp => {
     if (!resp.ok) {
       throw new Error(resp.statusText);
     }
--- src/index.js
+++ src/index.js
@@ -11,26 +11,27 @@ const userSeach = document.querySelector('#search-box');
 userSeach.addEventListener('input', debounce(onUserInput, DEBOUNCE_DELAY));
 
 function onUserInput(event) {
-  fetchCountries(event.target.value)
+  const inputValue = event.target.value.trim();
+  if (!inputValue) {
+    return;
+  }
+  fetchCountries(inputValue)
     .then(data => {
-      const capitals = data.map(elem => elem.name.official);
-console.log(capitals.length)
+      const capitals = data.map(elem => elem.name);
       if (capitals.length > 2 && capitals.length < 10) {
         countryInfo.innerHTML = '';
         countryList.innerHTML = '';
-        console.log(`>2 <10`);
         const marckupCountyList = data.map(
           elem =>
             `<li><img src="${elem.flags.svg}" alt="${elem.name.official} width="30px" height="30px"><h1>${elem.name.official}</h1></li>`
         );
         countryList.insertAdjacentHTML('beforeend', marckupCountyList.join(''));
-      } else if ((capitals.length === 1)) {
+      } else if (capitals.length === 1) {
         countryInfo.innerHTML = '';
         countryList.innerHTML = '';
-        console.log(`=1`);
         const markupCountyList = data.map(
           elem =>
-            `<li><img src="${elem.flags.svg}" alt="${elem.name.official} width="100px" height="100px"><h1>${elem.name.official}</h1></li>`
+            `<li><img src="${elem.flags.svg}" alt="${elem.name.official} width="100px" height="100px"><h2>${elem.name.official}</h2></li>`
         );
         const markUpCountryInfo = data.map(
           elem => ` <h2>Capital: ${elem.capital}</h2>
@@ -39,14 +40,21 @@ console.log(capitals.length)
         );
         countryList.insertAdjacentHTML('beforeend', markupCountyList.join(''));
         countryInfo.insertAdjacentHTML('beforeend', markUpCountryInfo.join(''));
+      } else if (userSeach.value === '') {
+        countryInfo.innerHTML = '';
+        countryList.innerHTML = '';
       } else {
-        console.log(`else`);
         Notiflix.Notify.info(
           'Too many matches found. Please enter a more specific name.'
         );
+        countryInfo.innerHTML = '';
+        countryList.innerHTML = '';
       }
     })
-    .catch(err =>
-      Notiflix.Notify.failure('Oops, there is no country with that name')
-    );
+    .catch(err => {
+      console.log(err);
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+      countryInfo.innerHTML = '';
+      countryList.innerHTML = '';
+    });
 }
